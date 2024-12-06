d62678a8d7e90fc94715e29ca77204d96ed048f9
Mariia Shuryha <mshurig2003@gmail.com>
2024-12-06 22:40:19 +0200

    Refactor timeConverter function

--- Variant1/js/script.js
+++ Variant1/js/script.js
@@ -62,23 +62,18 @@ function renderWeather(data) {
 }
 
 function timeConverter(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = [
+    const dateObj = new Date(UNIX_timestamp * 1000);
+    const months = [
       'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
     ];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes() >= 10 ? a.getMinutes() : '0' + a.getMinutes();
-    let time = {
-      date: date,
-      month: month,
-      year: year,
-      hour: hour,
-      min: min,
+  
+    return {
+      date: dateObj.getDate(),
+      month: months[dateObj.getMonth()],
+      year: dateObj.getFullYear(),
+      hour: dateObj.getHours(),
+      min: dateObj.getMinutes() >= 10 ? dateObj.getMinutes() : '0' + dateObj.getMinutes(),
     };
-    return time;
   }
 
 //   function getWindDirection(degree) {
